06a911ec25f31336831f643fedebedceefb9cc34
Felix Lamy <felix.lamy@gmail.com>
2017-09-09 12:20:24 +0100

    Created Function for Wheel Loads Calculations
    The wheel loads will be calculated using single mass system assumption.

---
+++ Calculate_wheelloads.m
@@ -0,0 +1,26 @@
+function [ OUT_Fz_FL, OUT_Fz_FR, OUT_Fz_RL, OUT_Fz_RR ] = Calculate_wheelLoads( Vehicle, glat, glong )
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%This function calculates all four wheel loads for a given vehicle under a
+%combined longitudinal acceleration and lateral acceleration. 
+
+%Reference: 
+
+%Lateral LT: RCVD p. 683, using the simplified Lateral Load transfer
+%calculations.
+
+%Longitudinal LT: RCVD p. 684
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%1. Lateral Load Tranfer
+
+FrontLLT = gLat
+
+%2. Longitudinal Load Transfer
+
+%3. Aerodynamic Loads
+
+
+
+end
+
