b05adf31b4f3c3f03b51ce8bd633759e1d9f764c
Felix Lamy <felix.lamy@gmail.com>
2017-09-09 21:36:35 +0100

    More work.

--- Calculate_wheelloads.m
+++ Calculate_wheelloads.m
@@ -8,7 +8,9 @@ function [ OUT_Fz_FL, OUT_Fz_FR, OUT_Fz_RL, OUT_Fz_RR ] = Calculate_wheelLoads( 
 
 %1. Calculate Lateral Load Tranfer deltas
 
-[FLLT, RLLT] = Ay * Calculate_LLT(Vehicle);
+[FLLT, RLLT] = Calculate_LLT(Vehicle);
+FLLT = Ay * FLLT;
+RLLT = Ay * RLLT;
 
 deltaLLT_FL = FLLT;
 deltaLLT_FR = -FLLT;
@@ -28,14 +30,11 @@ deltaLongLt_RR = LongLT;
 
 [deltaAeroLoad_FL, deltaAeroLoad_FR, deltaAeroLoad_RL, deltaAeroLoad_RR] = Calculate_AeroLoad(Vehicle, Test);
 
-
 %4. Compile all deltas
 
 OUT_Fz_FL = deltaLLT_FL + deltaLongLt_FL + deltaAeroLoad_FL;
 OUT_Fz_FR = deltaLLT_FR + deltaLongLt_FR + deltaAeroLoad_FR;
 OUT_Fz_RL = deltaLLT_RL + deltaLongLt_RL + deltaAeroLoad_RL;
 OUT_Fz_RR = deltaLLT_RR + deltaLongLt_RR + deltaAeroLoad_RR;
-
-
 end
 
